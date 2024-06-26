81f21220015eab7e14b4d1d9827014b9618da77d
DannyF74 <Dannytfirth@outlook.com>
2024-06-26 15:05:38 -0700

    Update Database_Challenge.py

--- Python_Challenges/Database_Challenge.py
+++ Python_Challenges/Database_Challenge.py
@@ -1,3 +1,13 @@
+# DATABASE CHALLENGE DETAILS
+# 1. Create a database table in RAM named Roster that includes the fields ‘Name’, ‘Species’ and ‘IQ.’
+#
+# 2. Populate your new table with the following values:
+#     (1, Jean-Baptiste Zorg, Human, 122), (2, Korben Dallas, Meat Popsicle, 100), (3, Ak'not, Mangalore, -5)
+#
+# 3. Update the Species of Korben Dallas to be Human.
+#
+# 4. Display the names and IQs of everyone in the table who is classified as Human.
+
 import sqlite3
 
 # Creating a new database stored in RAM
